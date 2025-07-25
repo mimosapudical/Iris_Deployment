ffb9219ffd336360bb9809a7934b16a48bed51c4
mimosapudical <153621177+mimosapudical@users.noreply.github.com>
2025-07-25 11:29:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ if st.button("Predict type of Iris"):
 
     image_path = class_to_image(predicted_class)
     st.write(image_path)
+    st.write(f"the path is: '{image_path} ")
     st.image(image_path, use_container_width=True)
 
 
